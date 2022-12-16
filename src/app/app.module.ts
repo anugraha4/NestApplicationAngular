3689a7e3ac0781fad7e5b5b574f67397b4cba5ca
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-12-17 04:13:34 +0530

    navbar admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,22 @@ import { AddemployeeComponent } from './addemployee/addemployee.component';
 import { EmployeeloginComponent } from './employeelogin/employeelogin.component';
 import { SecurityloginComponent } from './securitylogin/securitylogin.component';
 import { AddsecurityComponent } from './addsecurity/addsecurity.component';
-
+import { AdminNavbarComponent } from './admin-navbar/admin-navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AdminloginComponent
+  },
+  {
+    path:"addEmployee",
+    component:AddemployeeComponent
+  },
+  {
+    path:"addSecurity",
+    component:AddsecurityComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,11 +31,13 @@ import { AddsecurityComponent } from './addsecurity/addsecurity.component';
     AddemployeeComponent,
     EmployeeloginComponent,
     SecurityloginComponent,
-    AddsecurityComponent
+    AddsecurityComponent,
+    AdminNavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
